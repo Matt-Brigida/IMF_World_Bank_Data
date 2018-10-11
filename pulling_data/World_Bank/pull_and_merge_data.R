9dbf8597dbba23b58d97fd554b74bc052855c500
Matt Brigida <mdbrigida@gmail.com>
2018-10-10 20:19:19 -0400

    pulling data but many null

---
+++ pulling_data/World_Bank/pull_and_merge_data.R
@@ -0,0 +1,20 @@
+library(WDI)
+
+## get list of indicators-----
+WDIcache()
+## search list of indicators------
+WDIsearch(string = "financing", field = "name", short = TRUE, cache = NULL)
+
+## Current Account Balance as % of GDP
+cabp <- WDI(country = "all", indicator = "BN.CAB.XOKA.GD.ZS", start = 1900, end = 2019)
+
+## Financing via international capital markets (gross inflows, % of GDP)
+fvicfp <- WDI(country = "all", indicator = "CM.FIN.INTL.GD.ZS", start = 1900, end = 2019) ## no data
+
+## Domestic financing, total (% of GDP)
+dfp <- WDI(country = "all", indicator = "GB.FIN.DOMS.GD.ZS", start = 1900, end = 2019)  ## no data
+
+ffap <- WDI(country = "all", indicator = "GB.FIN.ABRD.GDP.ZS", start = 1900, end = 2019)  ## no data
+
+
+### merge data-----
