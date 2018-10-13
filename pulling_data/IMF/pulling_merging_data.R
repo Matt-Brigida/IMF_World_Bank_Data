95ad2aea459c0e421aeacd7088afe0386f1bc5d6
Matt Brigida <mdbrigida@gmail.com>
2018-10-12 20:29:15 -0400

    start pulling IMF data

---
+++ pulling_data/IMF/pulling_merging_data.R
@@ -0,0 +1,18 @@
+
+library("IMFData", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
+
+#  availableDB <- DataflowMethod()  ## doesn't work, problem on IMF end
+
+IFS.available.codes <- DataStructureMethod("IFS")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "USD")
+
+databaseID <- "IFS"
+startdate = "2001-01-01"
+enddate = "2016-12-31"
+checkquery = FALSE
+
+## Germany, Norminal GDP in Euros, Norminal GDP in National Currency
+queryfilter <- list(CL_FREA = "", CL_AREA_IFS = "GR", CL_INDICATOR_IFS = c("NGDP_EUR", "NGDP_XDC"))
+GR.NGDP.query <- CompactDataMethod(databaseID, queryfilter, startdate, enddate, 
+                                   checkquery)
+GR.NGDP.query$Obs[[1]]
