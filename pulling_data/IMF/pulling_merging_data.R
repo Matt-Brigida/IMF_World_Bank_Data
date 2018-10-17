7a864c056d1c7ae85caa7de70fde931e6c2fa9f9
Matt Brigida <mdbrigida@gmail.com>
2018-10-16 20:46:39 -0400

    pull IMF data

--- pulling_data/IMF/pulling_merging_data.R
+++ pulling_data/IMF/pulling_merging_data.R
@@ -5,14 +5,21 @@ library("IMFData", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
 
 IFS.available.codes <- DataStructureMethod("IFS")
 CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Current")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Balance of Payments")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Portfolio investment, Net acquisition of financial assets, Debt securities")
 
 databaseID <- "IFS"
-startdate = "2001-01-01"
+startdate = "1900-01-01"
 enddate = "2016-12-31"
 checkquery = FALSE
 
 ## Germany, Norminal GDP in Euros, Norminal GDP in National Currency
-queryfilter <- list(CL_FREA = "", CL_AREA_IFS = "GR", CL_INDICATOR_IFS = c("BXG_BP6_USD", "BMG_BP6_USD"))
-GR.NGDP.query <- CompactDataMethod(databaseID, queryfilter, startdate, enddate, 
-                                   checkquery)
-GR.NGDP.query$Obs[[1]]
+## queryfilter <- list(CL_FREA = "", CL_AREA_IFS = "GR", CL_INDICATOR_IFS = c("BXG_BP6_USD"))
+
+## spain, germany, "Portfolio investment, Net acquisition of financial assets, Debt securities"
+queryfilter <- list(CL_FREA = "", CL_AREA_IFS = c("ES", "GR"), CL_INDICATOR_IFS = c("BFPAD_BP6_USD"))
+
+BFPADBP6query <- CompactDataMethod(databaseID, queryfilter, startdate, enddate, checkquery)
+
+## look at first tome series, how to tell whether it is spain or germany---maybe have to query one country at a time
+BFPADBP6query$Obs[[1]]
--- pulling_data/World_Bank/pull_and_merge_data.R
+++ pulling_data/World_Bank/pull_and_merge_data.R
@@ -5,6 +5,7 @@ WDIcache()
 ## search list of indicators------
 WDIsearch(string = "financing", field = "name", short = TRUE, cache = NULL)
 WDIsearch(string = "current", field = "name", short = TRUE, cache = NULL)
+WDIsearch(string = "finance", field = "name", short = TRUE, cache = NULL)
 
 ## Current Account Balance as % of GDP
 cabp <- WDI(country = "all", indicator = "BN.CAB.XOKA.GD.ZS", start = 1900, end = 2019)
@@ -19,7 +20,16 @@ dfp <- WDI(country = "all", indicator = "GB.FIN.DOMS.GD.ZS", start = 1900, end =
 ffap <- WDI(country = "all", indicator = "GB.FIN.ABRD.GDP.ZS", start = 1900, end = 2019)  ## no data
 
 ## Military Expenditures (US $)
-mil <- WDI(country = "all", indicator = "MS.MIL.XPND.CN", start = 1900, end = 2019)  ## no data
+mil <- WDI(country = "all", indicator = "MS.MIL.XPND.CN", start = 1900, end = 2019)
+
+## Total deficit to be financed (US$, BoP)
+financed <- WDI(country = "all", indicator = "BN.CUR.ACTX.CD", start = 1900, end = 2019) ## no data
+
+## Firms using banks to finance investment (% of firms)" 
+fin_inv <- WDI(country = "all", indicator = "IC.FRM.BNKS.ZS", start = 1900, end = 2019)
+
+## "Present value of external debt (current US$)"
+pv_ex_debt <- WDI(country = "all", indicator = "DT.DOD.PVLX.CD", start = 1900, end = 2019)
 
 
 ### merge data-----
