dd58c5b6fb56c2f0d7c80573e98b89929590c041
Matt Brigida <mdbrigida@gmail.com>
2018-10-20 16:32:23 -0400

    can query all countries for ind vector and removed search

---
+++ pulling_data/IMF/get_indicators.R
@@ -0,0 +1,8 @@
+library("IMFData", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
+
+#  availableDB <- DataflowMethod()  ## doesn't work, problem on IMF end
+
+IFS.available.codes <- DataStructureMethod("IFS")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Current")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Balance of Payments")
+CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Portfolio investment, Net acquisition of financial assets, Debt securities")
--- pulling_data/IMF/pulling_merging_data.R
+++ pulling_data/IMF/pulling_merging_data.R
@@ -1,25 +1,32 @@
 
 library("IMFData", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
 
-#  availableDB <- DataflowMethod()  ## doesn't work, problem on IMF end
-
-IFS.available.codes <- DataStructureMethod("IFS")
-CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Current")
-CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Balance of Payments")
-CodeSearch(IFS.available.codes, "CL_INDICATOR_IFS", "Portfolio investment, Net acquisition of financial assets, Debt securities")
-
 databaseID <- "IFS"
 startdate = "1900-01-01"
 enddate = "2016-12-31"
 checkquery = FALSE
 
-## Germany, Norminal GDP in Euros, Norminal GDP in National Currency
-## queryfilter <- list(CL_FREA = "", CL_AREA_IFS = "GR", CL_INDICATOR_IFS = c("BXG_BP6_USD"))
+## country_list <- c("ES", "GR")
+country_list <- c("")  # all countries
+indicators <- c("BFPAD_BP6_USD", "BXG_BP6_USD")
+## indicators <- c("")  # cant do all indicators like so
+    
+queryfilter <- list(CL_FREA = "", CL_AREA_IFS = country_list, CL_INDICATOR_IFS = indicators)
 
-## spain, germany, "Portfolio investment, Net acquisition of financial assets, Debt securities"
-queryfilter <- list(CL_FREA = "", CL_AREA_IFS = c("ES", "GR"), CL_INDICATOR_IFS = c("BFPAD_BP6_USD"))
+data <- CompactDataMethod(databaseID, queryfilter, startdate, enddate, checkquery, tidy = TRUE)
 
-BFPADBP6query <- CompactDataMethod(databaseID, queryfilter, startdate, enddate, checkquery)
+## extract quarterly data for a particular country------
+getQ <- function(country_code, data){
+    tmp <- data[data$'@REF_AREA' == country_code, ]
+    tmp <- tmp[tmp$'@FREQ' == "Q", ]
+    tmp <- tmp[ , !(names(tmp) %in% c('@OBS_STATUS', '@FREQ'))]
+    return(tmp)
+}
 
-## look at first tome series, how to tell whether it is spain or germany---maybe have to query one country at a time
-BFPADBP6query$Obs[[1]]
+## extract annual data for a particular country------
+getA <- function(country_code, data){
+    tmp <- data[data$'@REF_AREA' == country_code, ]
+    tmp <- tmp[tmp$'@FREQ' == "A", ]
+    tmp <- tmp[ , !(names(tmp) %in% c('@OBS_STATUS', '@FREQ'))]
+    return(tmp)
+}
