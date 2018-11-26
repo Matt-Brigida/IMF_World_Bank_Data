b999545e5fc2fdb036c7048864e046cf11174485
Matt Brigida <mdbrigida@gmail.com>
2018-11-25 21:45:44 -0500

    started matching recommendations (not just unique country/years)

---
+++ pulling_data/merge_IMF_WB_REC_data.R
@@ -0,0 +1,18 @@
+
+### merge IMF and WB data-------
+
+imf <- readRDS("./IMF/IMFdata.rds")
+wb <- readRDS("./World_Bank/WB_data.rds")
+
+## get recommendations
+
+recs <- readRDS("./unique_country_year.rds")
+
+## merge-----
+
+data_all <- merge(imf, wb, by = c("year", "iso2c"), all = TRUE)
+## data_narrow <- merge(imf, wb, by = c("year", "iso2c"), all = FALSE)
+
+data_all$country <- toupper(data_all$country)
+
+recs_data <- merge(data_all, recs, by = c("year", "country"), all = FALSE)
