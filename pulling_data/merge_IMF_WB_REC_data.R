8927df39d0b9e8b0901a63b47cf91cd8bf8e12d5
Matt Brigida <mdbrigida@gmail.com>
2018-11-26 19:06:56 -0500

    merging with recommendations

--- pulling_data/merge_IMF_WB_REC_data.R
+++ pulling_data/merge_IMF_WB_REC_data.R
@@ -5,8 +5,7 @@ imf <- readRDS("./IMF/IMFdata.rds")
 wb <- readRDS("./World_Bank/WB_data.rds")
 
 ## get recommendations
-
-recs <- readRDS("./unique_country_year.rds")
+recs <- read.csv("../IMFRecommendations_fixed_line.txt", header = TRUE, sep = "|")
 
 ## merge-----
 
@@ -16,3 +15,9 @@ data_all <- merge(imf, wb, by = c("year", "iso2c"), all = TRUE)
 data_all$country <- toupper(data_all$country)
 
 recs_data <- merge(data_all, recs, by = c("year", "country"), all = FALSE)
+
+recs_data$recommendation <- as.character(recs_data$recommendation)
+
+library(SentimentAnalysis)
+
+sentiment <- analyzeSentiment(recs_data$recommendation)
