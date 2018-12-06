caa90d8b63a148fcfaae5827f4e5e2391447fac2
Matt Brigida <mdbrigida@gmail.com>
2018-12-06 17:06:37 -0500

    added WB merge function

--- pulling_data/merge_variable_into_full_data_set.R
+++ pulling_data/merge_variable_into_full_data_set.R
@@ -1,7 +1,7 @@
 ### load libraries----
 
 library("IMFData", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
-
+library(WDI)
 
 
 ### Merge IMF Variable
@@ -54,6 +54,31 @@ merge_imf <- function(indicator, backup = TRUE){
 
 ### Merge WB Variable
 
-merge_wb <- function(indicator){
+merge_wb <- function(indicator, backup = TRUE){
+
+    ## read in data-----
+    olddata <- readRDS("./merged_data.rds")
     
+    ## check if indicator is already in data set----
+    if (indicator %in% names(olddata)){
+        return("indicator already in data set")
+    }
+
+    ## pull wb indicator----
+    ind_data <- WDI(country = "all", indicator = indicator, start = 1900, end = 2019)
+
+    ## merge wb indicator----
+
+    new_data <- merge(ind_data, olddata, by = c("country", "year", "iso2c"), all.y = TRUE)
+
+    ## save new data set------
+
+    saveRDS(new_data, "merged_data.rds")
+
+    ## save old data as backup-----
+
+    if(backup == TRUE){
+        saveRDS(olddata, file = paste0("merged_data_backup_before_", indicator, ".rds"))
+        }
+
 }
