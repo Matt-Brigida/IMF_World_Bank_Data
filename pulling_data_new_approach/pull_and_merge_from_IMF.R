07230bc365a708c6d9b67d80b709b8ba14134bee
Matt Brigida <mdbrigida@gmail.com>
2019-06-06 19:01:53 -0400

    new approach for data set

---
+++ pulling_data_new_approach/pull_and_merge_from_IMF.R
@@ -0,0 +1,5 @@
+### New approach:
+### Pull all macro data from IMF, and merge with IMF recommendations/mentions.
+### Fill in missing data from World Bank or other source.
+### Data set will be a static csv.
+### See image in folder for set of data.
