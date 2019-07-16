c25fa8cc98b92f86c775f418a87e64c1e9273c4d
Matt Brigida <mdbrigida@gmail.com>
2019-07-16 18:07:06 -0400

    changed relative file paths

--- pulling_data/World_Bank/1_merge_WB_with_LHS_data/code_merge_LHS_with_all_WB_data.R
+++ pulling_data/World_Bank/1_merge_WB_with_LHS_data/code_merge_LHS_with_all_WB_data.R
@@ -2,7 +2,7 @@
 
 library(readr)
 
-WBdata <- read_csv("./WB_data.csv")
+WBdata <- read_csv("../WB_data.csv")
 names(WBdata)[1] <- "Country"
 names(WBdata)[2] <- "Year"
 WBdata$Country <- toupper(WBdata$Country)
@@ -12,7 +12,7 @@ WBdata[WBdata$Country == "NAMIBIA",]$iso2c <- "NAM"
 
 ### extract only years for which we have LHS data------
 
-LHS <- read_csv("../1_mentions_recs_with_isos/LHS_with_iso.csv")
+LHS <- read_csv("../../1_mentions_recs_with_isos/LHS_with_iso.csv")
 dim(LHS)
 
 ### Merge LHS with WB data------
