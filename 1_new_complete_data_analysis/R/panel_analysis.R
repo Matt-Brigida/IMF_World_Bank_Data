21e6aca2031657c6ca73678e9335f8bbeef9bd75
Matt Brigida <mdbrigida@gmail.com>
2019-08-04 20:06:02 -0400

    added list of variables and def to analysis code

--- 1_new_complete_data_analysis/R/panel_analysis.R
+++ 1_new_complete_data_analysis/R/panel_analysis.R
@@ -17,7 +17,20 @@ d <- d %>%
 
 ### explanatory variables-----
 
-vars <- c("BN.CAB.XOKA.GD.ZS", "NY.GDP.PCAP.KD.ZG", "SL.TLF.CACT.ZS")#, "GB.XPD.RSDV.GD.ZS")
+vars <- c("BN.CAB.XOKA.GD.ZS", # Current Account Balance as % of GDP
+          "IC.FRM.BNKS.ZS", # Firms using banks to finance investment (% of firms)
+          "BX.KLT.DINV.WD.GD.ZS", # Foreign direct investment, net inflows (% of GDP) (Replace Foreign direct investment net)
+          "BX.TRF.PWKR.DT.GD.ZS", # Personal remittances, received (% of GDP)
+          "BG.GSR.NFSV.GD.ZS", # Trade in services (% of GDP)
+          "NV.AGR.TOTL.ZS", # Agriculture, forestry, and fishing, value added (% of GDP)
+          "NE.EXP.GNFS.ZS", # Exports of goods and services (% of GDP)
+          "NY.GDP.MKTP.KD.ZG", # GDP growth (annual %)
+          "NY.GDP.PCAP.KD.ZG", # GDP per capita growth (annual %)
+          "NY.GDS.TOTL.ZS", # Gross domestic savings (% of GDP)
+          "NE.IMP.GNFS.ZS", # Imports of goods and services (% of GDP)
+          "SL.TLF.CACT.ZS", # Labor force participation rate, total (% of total population ages 15+) (modeled ILO estimate)
+          "BCG_GALM_G01_XDC" # Central government debt, total (% of GDP)
+          )
 
 mentions_form <- as.formula(paste("sMentions ~", paste(vars, collapse="+")))
 
@@ -33,3 +46,7 @@ ed <- subset(d, Topic == "External Debt")
 ed <- pdata.frame(ed, index = c("Country.x", "Year"), drop.index=TRUE, row.names=TRUE)
 
 SM_ED_results <- plm(mentions_form, data = ed, model = "between", effect = "individual")
+SM_ED_results <- plm(mentions_form, data = ed, model = "random")
+
+
+SR_ED_results <- plm(recommendations_form, data = ed, model = "between", effect = "individual")
