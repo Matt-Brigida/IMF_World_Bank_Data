ac99e95e53af71108177129b07bef4697a3f7140
Matt Brigida <mdbrigida@gmail.com>
2019-09-09 23:02:11 -0400

    python analysis working but check results

--- 1_new_complete_data_analysis/python/panel_analysis.py
+++ 1_new_complete_data_analysis/python/panel_analysis.py
@@ -9,7 +9,8 @@ data = pd.read_csv("./1_new_complete_data_analysis/merge_new_data/updated_analys
 
 ### create scaled mentions and recommendations------
 
-data.groupby(['Country.x', 'Year'])['Mentions'] / (data.groupby(['Country.x', 'Year']).sum()['Mentions'])
+data['sMentions'] = data.groupby(['Country.x', 'Year'])['Mentions'].apply(lambda x: x.astype(float)/x.sum())
+data['sRecommendations'] = data.groupby(['Country.x', 'Year'])['Recommendations'].apply(lambda x: x.astype(float)/x.sum())
 
 
 Year = pd.Categorical(data.Year)
@@ -21,21 +22,29 @@ data['Year'] = Year
 
 exog_vars = [
 "BN.CAB.XOKA.GD.ZS", # Current Account Balance as % of GDP
-"IC.FRM.BNKS.ZS", # Firms using banks to finance investment (% of firms)
-"BX.KLT.DINV.WD.GD.ZS", # Foreign direct investment, net inflows (% of GDP) (Replace Foreign direct investment net)
-"BX.TRF.PWKR.DT.GD.ZS", # Personal remittances, received (% of GDP)
-"BG.GSR.NFSV.GD.ZS", # Trade in services (% of GDP)
-"NV.AGR.TOTL.ZS", # Agriculture, forestry, and fishing, value added (% of GDP)
-"NE.EXP.GNFS.ZS", # Exports of goods and services (% of GDP)
+# "IC.FRM.BNKS.ZS", # Firms using banks to finance investment (% of firms)
+# "BX.KLT.DINV.WD.GD.ZS", # Foreign direct investment, net inflows (% of GDP) (Replace Foreign direct investment net)
+# "BX.TRF.PWKR.DT.GD.ZS", # Personal remittances, received (% of GDP)
+# "BG.GSR.NFSV.GD.ZS", # Trade in services (% of GDP)
+# "NV.AGR.TOTL.ZS", # Agriculture, forestry, and fishing, value added (% of GDP)
+# "NE.EXP.GNFS.ZS", # Exports of goods and services (% of GDP)
 "NY.GDP.MKTP.KD.ZG", # GDP growth (annual %)
-"NY.GDP.PCAP.KD.ZG", # GDP per capita growth (annual %)
-"NY.GDS.TOTL.ZS", # Gross domestic savings (% of GDP)
-"NE.IMP.GNFS.ZS", # Imports of goods and services (% of GDP)
+# "NY.GDP.PCAP.KD.ZG", # GDP per capita growth (annual %)
+# "NY.GDS.TOTL.ZS", # Gross domestic savings (% of GDP)
+# "NE.IMP.GNFS.ZS", # Imports of goods and services (% of GDP)
 "SL.TLF.CACT.ZS", # Labor force participation rate, total (% of total population ages 15+) (modeled ILO estimate)
 "BCG_GALM_G01_XDC" # Central government debt, total (% of GDP)
 ]
 
-exog = sm.add_constant(data[exog_vars])
-mod = RandomEffects(data.Mentions, exog)
-re_res = mod.fit()
-print(re_res)
+
+### External Debt------
+
+data_ed = data.query('Topic == "External Debt"')
+exog_ed = sm.add_constant(data_ed[exog_vars])
+
+
+## fixed effects-----
+fe_mod = PanelOLS(data_ed.sMentions, exog_ed, entity_effects=True, time_effects=True)
+# fe_res_ed = fe_mod.fit(cov_type='clustered', cluster_entity=True)
+fe_res_ed = fe_mod.fit()
+print(fe_res_ed)
